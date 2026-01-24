db85eb5d70eec37ad0839d17fef371b4c53771e3
harsh shinde <shindeharsh.dev@gmail.com>
2026-01-24 17:49:42 +0530

    added translation

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // 👈 This forces the folder name to be "build"
+  },
 });
